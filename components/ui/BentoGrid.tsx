978c119fb11a2aadb5ad43f4fc6b54a412695ce8
Ganesh Lonare <ganeshlonare311@gmail.com>
2025-04-20 17:15:16 +0530

    grid change

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "ganeshlonareofficial@gmail.com";
+    const text = "ganeshlonare311@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
